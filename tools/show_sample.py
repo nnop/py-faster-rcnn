ab0e030e81481c2d7c57e346196f61e8c0627154
nnop <nnop>
2017-08-21 10:01:57 +0800

    add show sample

---
+++ tools/show_sample.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import json
+import cv2
+import os.path as osp
+import sys
+import argparse
+
+def draw_bbox(ax, bbox, text='', color='r', linewidth=1):
+    xmin, ymin, xmax, ymax = bbox
+    coords = (xmin, ymin), xmax-xmin+1, ymax-ymin+1
+    ax.add_patch(plt.Rectangle(*coords, fill=False, 
+            edgecolor=color, linewidth=linewidth))
+    ax.text(xmin, ymin, text, bbox={'facecolor': color, 'alpha': 0.5})
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--image-path', required=True)
+    parser.add_argument('--json-path', required=True)
+
+    image_path = args.image_path
+    json_path = args.json_path
+
+    im = cv2.imread(image_path)[:, :, [2, 1, 0]]
+    plt.imshow(im)
+    with open(json_path) as f:
+        info = json.load(f)
+    ax = plt.gca()
+    for psn in info['persons']:
+        body_bbox = psn['body']['bbox']
+        body_pose = psn['body']['label']
+        draw_bbox(ax, body_bbox, text=body_pose[:3], color='r', linewidth=2)
+        head_bbox = psn['head']['bbox']
+        head_pose = psn['head']['label']
+        draw_bbox(ax, head_bbox, text=head_pose[:3], color='g', linewidth=1)
+    save_fn = osp.splitext(osp.split(image_path)[1])[0] + '_vis.png'
+    plt.savefig(save_fn)
+    print 'save to', save_fn
